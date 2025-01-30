47298acea478e0b8ccba43e64dcc6e584483c050
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2025-01-30 11:14:35 +0300

    Update for 2025

--- ccd.py
+++ ccd.py
@@ -1,5 +1,10 @@
+import argparse
 from astropy.io import fits
 from lsp import lstsq
 
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', type=str, help='Path to ccd.fits')
+
 if __name__ == "__main__":
-    pass
+    args = parser.parse_args()
+    print(args.filename)
